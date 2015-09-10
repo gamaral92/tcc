5487132cc2fea4a2c10acdb8b554c2dbf9f375e2
Gabriel <Gabriel@192.168.1.206>
2015-09-10 18:39:29 -0300

    INICIO

--- src/imagem/Regiao.java
+++ src/imagem/Regiao.java
@@ -8,6 +8,9 @@ package imagem;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import javax.imageio.ImageIO;
 
 /**
@@ -25,42 +28,97 @@ public class Regiao {
                     matriz[x][y] = image1.getRGB(x, y) & 0xFF;
                 }
             }
+            Set<String> set = new HashSet<>();
             for (int y = 0; y < image1.getHeight(); y++) {
                 for (int x = 0; x < image1.getWidth(); x++) {
-                    System.out.print(matriz[x][y] + "\t");
+                    if(matriz[x][y] > 40){
+                        set.add(getRegiao(x, y).toUpperCase());
+                    }
                 }
-                System.out.println("");
             }
+            for (Iterator<String> iterator = set.iterator(); iterator.hasNext();) {
+                String next = iterator.next();
+                System.out.print(next);
+            }
+            System.out.println("");
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
-    public String getRegiao(int x, int y) {
+    public static String getRegiao(int x, int y) {
         if ((x >= 0) && (x < 66.25)) {
             if ((y >= 0) && (y < 59.875)) {
                 return "a1";
             } else if ((y >= 59.875) && (y < 119.75)) {
+                return "b1";
+            } else if ((y >= 119.75) && (y < 179.625)) {
+                return "c1";
+            } else if ((y >= 179.625) && (y < 239.5)) {
+                return "d1";
+            } else if ((y >= 239.5) && (y < 299.375)) {
+                return "e1";
+            } else if ((y >= 299.375) && (y < 359.25)) {
+                return "f1";
+            } else if ((y >= 359.25) && (y < 419.125)) {
+                return "g1";
+            } else if ((y >= 419.125) && (y < 479)) {
+                return "h1";
+            }
+        } else if ((x >= 66.25) && (x < 132.5)) {
+            if ((y >= 0) && (y < 59.875)) {
+                return "a2";
+            } else if ((y >= 59.875) && (y < 119.75)) {
                 return "b2";
+            } else if ((y >= 119.75) && (y < 179.625)) {
+                return "c2";
+            } else if ((y >= 179.625) && (y < 239.5)) {
+                return "d2";
+            } else if ((y >= 239.5) && (y < 299.375)) {
+                return "e2";
+            } else if ((y >= 299.375) && (y < 359.25)) {
+                return "f2";
+            } else if ((y >= 359.25) && (y < 419.125)) {
+                return "g2";
+            } else if ((y >= 419.125) && (y < 479)) {
+                return "h2";
+            }
+        } else if ((x >= 132.5) && (x < 198.75)) {
+            if ((y >= 0) && (y < 59.875)) {
+                return "a3";
+            } else if ((y >= 59.875) && (y < 119.75)) {
+                return "b3";
             } else if ((y >= 119.75) && (y < 179.625)) {
                 return "c3";
             } else if ((y >= 179.625) && (y < 239.5)) {
+                return "d3";
+            } else if ((y >= 239.5) && (y < 299.375)) {
+                return "e3";
+            } else if ((y >= 299.375) && (y < 359.25)) {
+                return "f3";
+            } else if ((y >= 359.25) && (y < 419.125)) {
+                return "g3";
+            } else if ((y >= 419.125) && (y < 479)) {
+                return "h3";
+            }
+        } else if ((x >= 198.75) && (x < 265)) {
+            if ((y >= 0) && (y < 59.875)) {
+                return "a4";
+            } else if ((y >= 59.875) && (y < 119.75)) {
+                return "b4";
+            } else if ((y >= 119.75) && (y < 179.625)) {
+                return "c4";
+            } else if ((y >= 179.625) && (y < 239.5)) {
                 return "d4";
             } else if ((y >= 239.5) && (y < 299.375)) {
-                return "e5";
+                return "e4";
             } else if ((y >= 299.375) && (y < 359.25)) {
-                return "f6";
+                return "f4";
             } else if ((y >= 359.25) && (y < 419.125)) {
-                return "g7";
+                return "g4";
             } else if ((y >= 419.125) && (y < 479)) {
-                return "h8";
+                return "h4";
             }
-        } else if ((x >= 66.25) && (x < 132.5)) {
-
-        } else if ((x >= 132.5) && (x < 198.75)) {
-
-        } else if ((x >= 198.75) && (x < 265)) {
-
         }
         return null;
     }
